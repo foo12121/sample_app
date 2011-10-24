5f521e92f6ef1f60c75a239e364af9a373525d13
okalim <okalim@velos.com>
2011-10-23 20:29:02 -0700

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,12 @@ validates :email, :presence   => true,
     return user if user.has_password?(submitted_password)
   end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+ 
+
   private
 
     def encrypt_password
